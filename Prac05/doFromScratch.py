3729862e0a8922d820a0142d2594424ecf58b04b
nishiluke <nishiluke@gmail.com>
2016-08-29 09:58:25 +1000

    finished doFromScratch finished Prac05

---
+++ Prac05/doFromScratch.py
@@ -0,0 +1,15 @@
+"""
+the quick brown fox jumped over the lazy dog
+"""
+
+inputText = str(input("Text: "))
+inputDict = {}
+for word in inputText.split():
+    if word not in inputDict:
+        inputDict[word] = 1
+    else:
+        inputDict[word] += 1
+
+for item in inputDict:
+    print("{} : {}".format(item, inputDict[item]))
+
